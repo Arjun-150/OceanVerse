e77d5a8027175da951d6a3a27d30e06b14e7d640
Arjun S <arjunsankar150@gmail.com>
2024-12-17 22:11:58 +0530

    Create OVQ2.py

---
+++ ocean PART_1/OVQ2.py
@@ -0,0 +1,6 @@
+# Take as input a number n and print the square, cube and 2n of the number (1 point).
+
+n=int(input("enter a number: "))
+print('square of number n =', n**2)
+print('cube of number n =', n**3)
+print('2^n =', 2**n)
