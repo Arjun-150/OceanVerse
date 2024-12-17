caaf363f6970b56de417b428bd65b90b65fdb53b
Arjun S <arjunsankar150@gmail.com>
2024-12-17 22:30:53 +0530

    Create q33.py

---
+++ ocean PART_3/q33.py
@@ -0,0 +1,14 @@
+'''
+Write a function called add that takes two numbers as arguments and returns their sum. (1 point)
+'''
+
+def add(a,b):
+    c=a+b
+    print('the sum is :',c )
+    return c
+
+a=int(input('enter the first number : '))
+b=int(input('enter the second number : '))
+add(a,b)
+
+          
