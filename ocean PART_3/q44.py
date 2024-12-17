ef79e909259395b238c434f2437a3cfac218ccb2
Arjun S <arjunsankar150@gmail.com>
2024-12-17 22:35:24 +0530

    Create q44.py

---
+++ ocean PART_3/q44.py
@@ -0,0 +1,5 @@
+'''
+Consider the data of Height and Weight of 25,000 people as available here. Is the data correlated? What is the correlation? 
+Can you plot the points and fit a line? You are free to use any built in functions, but you should know what the functions are doing. 
+Correlation is a statistical concept which you may know from your high school, we only expect you to understand this concept at an intuitive level. (5 point)
+'''
